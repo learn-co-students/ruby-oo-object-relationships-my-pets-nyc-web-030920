5600dcf61d177c2c9bf67778e9afcf24eedd9526
Asilver-jpg <asilver7493@gmail.com>
2020-03-11 21:13:42 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all =[]
+  def initialize(name, owner)
+@name= name
+@owner=owner
+@mood = "nervous"
+    Cat.all << self
   # code goes here
 end
+
+def self.all
+@@all
+end
+end
+
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all =[]
+  def initialize(name, owner)
+@name= name
+@owner=owner
+@mood = "nervous"
+    Dog.all << self
   # code goes here
 end
+
+def self.all
+@@all
+end
+end
+
